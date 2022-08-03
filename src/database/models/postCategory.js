bd2f798f073e435913adb2db2fac3ac5aea40342
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-08-03 17:36:56 -0300

    Atende requisito 11.

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,36 @@
+const sequelize = require("sequelize");
+
+const PostCategory = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory', {
+        postId: {
+           type: DataTypes.INTEGER,
+           primaryKey: true,
+        },
+        categoryId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+         },
+    },
+    {
+        timestamps: false,
+        underscored: true,
+        tableName: 'PostCategories',
+    });
+    PostCategory.associate = (db) => {
+        db.Category.belongsToMany(db.BlogPost, {
+            as: 'BlogPosts',
+            through: PostCategory,
+            foreignKey: 'categoryId',
+            otherKey: 'postId',
+        });
+        db.BlogPost.belongsToMany(db.Category, {
+            as: 'Categories',
+            through: PostCategory,
+            foreignKey: 'postId',
+            otherKey: 'categoryId',
+        });
+    }
+    return PostCategory;
+};
+
+module.exports = PostCategory;
